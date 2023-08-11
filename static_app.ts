eb420845e3d46c2b90c2cf15b2c010e421d5e3f7
Alejandro <10840260+alejandro913@users.noreply.github.com>
2023-08-11 13:21:15 -0500

    adding the initial css and ts code

---
+++ static_app.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import './staticpagecss.css'; // Make sure to create this CSS file
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="message">Hello, this is the start of a beginner project</h1>
+    </div>
+  );
+}
+
+export default App;
